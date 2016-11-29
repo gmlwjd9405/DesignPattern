8dd70fa083ea7a3fa68e12278774e2ffcc7c8e83
권희정 <gmlwjd9405@naver.com>
2016-11-29 21:54:24 +0900

    modify package

---
+++ DP_Week9_Spring/src/main/java/Client.java
@@ -0,0 +1,39 @@
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import robot.Robot;
+
+public class Client {
+	public static void main(String[] args) {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+		Robot taekwonV = (Robot) context.getBean("taekwonV");
+		System.out.println(taekwonV.getName());
+		taekwonV.move();
+		taekwonV.attack();
+
+		System.out.println();
+
+		Robot atom = (Robot) context.getBean("atom");
+		System.out.println(atom.getName());
+		atom.move();
+		atom.attack();
+
+		System.out.println();
+
+		Robot sungard = (Robot) context.getBean("sungard");
+		System.out.println(sungard.getName());
+		sungard.move();
+		sungard.attack();
+
+		
+		// Robot robot1 = new Atom("atom");
+		// robot1.setAttackStrategy(new Punch());
+		// robot1.setMovingStrategy(new Walk());
+		//
+		// robot1.move();
+		// robot1.attack();
+	}
+}
