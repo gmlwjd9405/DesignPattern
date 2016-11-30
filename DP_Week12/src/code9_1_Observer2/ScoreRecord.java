8e4b49a81517afc4585aba904c852c10eb59ae1d
권희정 <gmlwjd9405@naver.com>
2016-11-30 15:23:10 +0900

    change ScoreRecord class -> extends Subject

--- DP_Week12/src/code9_1_Observer2/ScoreRecord.java
+++ DP_Week12/src/code9_1_Observer2/ScoreRecord.java
@@ -3,23 +3,11 @@ package code9_1_Observer2;
 import java.util.List;
 import java.util.ArrayList;
 
-public class ScoreRecord {
+public class ScoreRecord extends Subject{
 	/* 3. 출력형태 2개를 가질 때 */
 	private List<Integer> scores = new ArrayList<Integer>(); // 점수를 저장함
 	private List<Observer> observers = new ArrayList<Observer>(); // 출력 형태에 대한
 																	// observer
-
-	/** 여러 개에 대한 setter */
-	public void addObserver(Observer observer) {
-		observers.add(observer);
-	}
-
-	/** List<Observer>객체들의 update를 호출 */
-	public void notifyObservers() {
-		for (Observer o : observers) {
-			o.update();
-		}
-	}
 
 	/** 새로운 점수를 추가 */
 	public void addScore(int score) {
